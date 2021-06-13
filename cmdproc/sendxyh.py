ed00cb57077623012ae46096b2113595bbc43356
veronicahe <82675868+veronicahe@users.noreply.github.com>
2021-06-13 09:33:47 -0400

    6/13作业

--- cmdproc/sendxyh.py
+++ cmdproc/sendxyh.py
@@ -5,46 +5,53 @@ import pandas as pd
 import pandas_datareader as pdr
 import datetime
 
-start_thirteen = datetime.date.today() - datetime.timedelta(days=13)
-start_fifty = datetime.date.today() - datetime.timedelta(days=50)
-start_twohundred = datetime.date.today() - datetime.timedelta(days=200)
+stock_data = [['spy',13,50],['qqq',13,50,200],['^SPX',55,200]]
+symbol = [item[0] for item in stock_data]
+ma_1 = [item[1] for item in stock_data]
+ma_2 = [item[2] for item in stock_data]
+ma_3 = stock_data[1][3]
+
+start = datetime.date.today() - datetime.timedelta(days=365)
 end = datetime.date.today()
 
-symbol = 'spy'
-df = pdr.get_data_yahoo(symbol.upper(),start=start_thirteen,end=end)
-spy_thirteen_average = round(df['Close'].mean(),2)
-df = pdr.get_data_yahoo(symbol.upper(),start=start_fifty,end=end)
-spy_fifty_average = round(df['Close'].mean(),2)
-df = pdr.get_data_yahoo(symbol.upper(),start=start_twohundred,end=end)
-spy_twohundred_average = round(df['Close'].mean(),2)
-df = pdr.get_data_yahoo(symbol.upper(),start=end,end=end)
-spy_today_close = round(df['Close'].mean(),2)
-spy_today_low = round(df['Low'].mean(),2)
-spy_today_high = round(df['High'].mean(),2)
+df = pdr.get_data_yahoo(symbol[0].upper(),start=start,end=end)
+spy_average_ma_1 = round(df.tail(int(ma_1[0]))['Close'].mean(),2)
+spy_average_ma_2 = round(df.tail(int(ma_2[0]))['Close'].mean(),2)
+spy_today_close = round(df.tail(1)['Close'].mean(),2)
+spy_today_low = round(df.tail(1)['Low'].mean(),2)
+spy_today_high = round(df.tail(1)['High'].mean(),2)
 
-symbol = 'qqq'
-df = pdr.get_data_yahoo(symbol.upper(),start=start_thirteen,end=end)
-qqq_thirteen_average = round(df['Close'].mean(),2)
-df = pdr.get_data_yahoo(symbol.upper(),start=start_fifty,end=end)
-qqq_fifty_average = round(df['Close'].mean(),2)
-df = pdr.get_data_yahoo(symbol.upper(),start=start_twohundred,end=end)
-qqq_twohundred_average = round(df['Close'].mean(),2)
-df = pdr.get_data_yahoo(symbol.upper(),start=end,end=end)
-qqq_today_close = round(df['Close'].mean(),2)
-qqq_today_low = round(df['Low'].mean(),2)
-qqq_today_high = round(df['High'].mean(),2)
+df = pdr.get_data_yahoo(symbol[1].upper(),start=start,end=end)
+qqq_average_ma_1 = round(df.tail(int(ma_1[1]))['Close'].mean(),2)
+qqq_average_ma_2 = round(df.tail(int(ma_2[1]))['Close'].mean(),2)
+qqq_average_ma_3 = round(df.tail(int(ma_3))['Close'].mean(),2)
+qqq_today_close = round(df.tail(1)['Close'].mean(),2)
+qqq_today_low = round(df.tail(1)['Low'].mean(),2)
+qqq_today_high = round(df.tail(1)['High'].mean(),2)
+
+df = pdr.get_data_yahoo(symbol[2].upper(),start=start,end=end)
+SPX_average_ma_1 = round(df.tail(int(ma_1[2]))['Close'].mean(),2)
+SPX_average_ma_2 = round(df.tail(int(ma_2[2]))['Close'].mean(),2)
+SPX_today_close = round(df.tail(1)['Close'].mean(),2)
+SPX_today_low = round(df.tail(1)['Low'].mean(),2)
+SPX_today_high = round(df.tail(1)['High'].mean(),2)
 
 bot_reply = f"""
 当日天相
 SPY价格： {spy_today_close} ({spy_today_low}-{spy_today_high})
-13周期均价： {spy_thirteen_average}
-50周期均价： {spy_fifty_average}
+13周期均价： {spy_average_ma_1}
+50周期均价： {spy_average_ma_2}
 
 QQQ价格： {qqq_today_close} ({qqq_today_low}-{qqq_today_high})
-13周期均价： {qqq_thirteen_average}
-50周期均价： {qqq_fifty_average}
-200周期均价： {qqq_twohundred_average}
+13周期均价： {qqq_average_ma_1}
+50周期均价： {qqq_average_ma_2}
+200周期均价：{qqq_average_ma_3}
+
+^SPX价格： {SPX_today_close} ({SPX_today_low}-{SPX_today_high})
+55周期均价： {SPX_average_ma_1}
+200周期均价： {SPX_average_ma_2}
 """
+print(bot_reply)
 
 def sendxyh(update: Update, _: CallbackContext) -> None:
     bot = update.effective_message.bot
